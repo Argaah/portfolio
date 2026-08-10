82c3d46e34c3f07f8e8e1a26098d2951b603d5a7
adr6hmn <diandiarga@gmail.com>
2026-08-11 00:10:23 +0800

    update projectss

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ mode.addEventListener('mouseenter', ()=> {
 
 })
 
-window.addEventListener('mouseover', (e)=> {
+window.addEventListener('mousemove', (e)=> {
     const rect = character.getBoundingClientRect()
     
     const centerX = rect.left + rect.width / 2
